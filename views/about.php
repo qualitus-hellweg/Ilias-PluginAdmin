9f0c2f1208f1a1d6539a05cde66476efc3160c59
chellweg <hellweg@qualitus.de>
2023-12-11 23:13:10 +0100

    debugging

--- views/about.php
+++ views/about.php
@@ -7,10 +7,15 @@ if( ! defined( 'I_WAS_CALLED_FROM_INDEX' ) ) {
     $baseUrl = $_SERVER[ 'PHP_SELF' ];
 
     $repo = new Repoinformation( __DIR__ . '/..' );
-
+    
+    $behind = 'up to date';
+    $temp = $repo->getBehind();
+    if( strlen( $temp ) > 0 ) {
+        $behind = $temp;
+    }
     echo '<div class="how-to">';
     echo 'hash  : ' . $repo->getHash() . '<br />';
-    echo 'behind: ' . $repo->getBehind() . '<br />';
+    echo 'behind: ' . $behind . '<br />';
     echo '<a href="' . $baseUrl . '?pullme=1&cmd=1&path=1">pull me</a>';
     
     
