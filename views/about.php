3d4a7f28c62c2da4a5115a595b81a135d461322f
chellweg <hellweg@qualitus.de>
2023-12-11 23:05:34 +0100

    debugging

--- views/about.php
+++ views/about.php
@@ -2,3 +2,15 @@
 if( ! defined( 'I_WAS_CALLED_FROM_INDEX' ) ) {
     die();
 }
+    echo '<h1 class="main-header">about ilPluginAdmin</h1>';
+
+    $baseUrl = $_SERVER[ 'PHP_SELF' ];
+
+    $repo = new Repoinformation( __DIR__ . '/..' );
+
+    echo 'hash  : ' . $repo->getHash() . '<br />';
+    echo 'behind: ' . $repo->getBehind() . '<br />';
+    echo '<a href="' . $baseUrl . 'pullme=1">pull me</a>';
+    
+    
+    echo '<hr />';
